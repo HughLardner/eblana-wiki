25ecd3a63f23d21d3ef3d862838979ac46c9a7a2
Hugh <hlardner@salesforce.com>
2017-05-25 17:06:49 +0100

    Email

--- LocalSettings.php
+++ LocalSettings.php
@@ -135,3 +135,12 @@ wfLoadSkin( 'Vector' );
 # End of automatically generated settings.
 # Add more configuration options below.
 
+$wgSMTP = array(
+'host' => getenv('EMAIL_HOST'), //could also be an IP address
+'IDHost' => getenv('EMAIL_ID_HOST'),
+'port' => getenv('EMAIL_PORT'),
+'auth' => true,
+'username' => getenv('EMAIL_USER'),
+'password' => getenv('EMAIL_PASSWORD')
+);
+
